437ca0e2bd13f03e1fadf6641272dfa9d08f8055
elisa.adriaenssens.etu <elisa.adriaenssens.etu@frene09.iut-infobio.priv.univ-lille1.fr>
2021-09-01 16:42:55 +0200

    enum enrichie

--- src/pokehmon/Choix.java
+++ src/pokehmon/Choix.java
@@ -2,7 +2,13 @@ package pokehmon;
 
 public enum Choix {
 	
-	BALL, FUITE, COOKIE, CAILLOU;
+	BALL(1), COOKIE(2), CAILLOU(3), FUITE(4);
+	
+	private int nb;
+	
+	private Choix(int nb) {
+		this.nb = nb;
+	}
 	
 
 }

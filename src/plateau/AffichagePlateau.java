6c6ab2ea534b01d7621846c354246b65f13ee2e5
Hugo Paree <hugo.paree.etu@univ-lille.fr>
2021-09-02 08:10:38 +0200

    modification visuel

--- src/plateau/AffichagePlateau.java
+++ src/plateau/AffichagePlateau.java
@@ -2,6 +2,8 @@ package plateau;
 
 public class AffichagePlateau {
 	public static void afficherPlateau(Plateau p) {
+		
+		System.out.print("\u001b[48;38;5;0m");
 		char[][] niveau = p.getNiveau();
 		Joueur joueur = p.getJoueur();
 		int nblig = 0;
@@ -10,16 +12,65 @@ public class AffichagePlateau {
 				nblig = niveau[i].length;
 			}
 		}
-		
+
 		for (int l = 0; l < niveau[nblig].length; l++) {
-			for (int c = 0; c < niveau.length; c++) {
-				if(joueur.getX()==c && joueur.getY()==l) {
-					System.out.print("J");
-				}else {
-					System.out.print(niveau[c][l]);
+			for (int i = 0; i < 2; i++) {
+				for (int c = 0; c < niveau.length; c++) {
+					modification_couleur(niveau, l, c);
+						
+					affichage_case(niveau, joueur, l, c, i);
+
+					System.out.print("\u001b[48;38;5;0m");
 				}
+				System.out.println();
 			}
-			System.out.println();
+		}
+	}
+
+	private static void affichage_case(char[][] niveau, Joueur joueur, int l, int c, int p) {
+		if(joueur.getX()==c && joueur.getY()==l) {
+			if(p==0) {
+				System.out.print("\u001b[38;5;255m O");
+			}else {
+				System.out.print("\u001b[38;5;1m©\u001b[38;5;255m╣");
+			}
+		}else {
+			switch (niveau[c][l]) {
+			case '/':
+				System.out.print("▓▓");
+				break;
+			case 'H':
+				System.out.print("░░");
+				break;
+			case 'E':
+				System.out.print("  ");
+				break;
+			case ' ':
+				System.out.print("  ");
+				break;
+			default:
+				System.out.print(niveau[c][l]);
+				break;
+			}
+		}
+	}
+
+	private static void modification_couleur(char[][] niveau, int l, int c) {
+		switch (niveau[c][l]) {
+		case '/':
+			System.out.print("\u001b[38;5;88m\u001b[48;5;172m");
+			break;
+		case 'H':
+			System.out.print("\u001b[38;5;28m\u001b[48;5;34m");
+			break;
+		case 'E':
+			System.out.print("\u001b[48;5;21m");
+			break;
+		case ' ':
+			System.out.print("\u001b[48;5;136m");
+			break;
+		default:
+			break;
 		}
 	}
 }
